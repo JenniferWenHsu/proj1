afa5efb5631e8a8cb62e86a80905da8e9ab17739
Jennifer Hsu <jennifer.wen123@gmail.com>
2016-03-18 21:08:25 -0700

    complete question 5

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -8,11 +8,35 @@ class PokemonsController < ApplicationController
 
 	def damage
 		@pokemon = current_trainer.pokemons.find(params[:id])
-		@pokemon.health = @pokemon.health -20
-		if @pokemon.health <=0
+		@pokemon.health = @pokemon.health - 20
+		if @pokemon.health <= 0
 			@pokemon.destroy
 		else
 			@pokemon.save
 		end
+		redirect_to trainer_path(current_trainer.id)
 	end
+
+  	def create
+  		@pokemon = Pokemon.create(pokemon_params)
+    	@pokemon.health = 100
+    	@pokemon.level = 1
+    	@pokemon.trainer = current_trainer
+    	if @pokemon.save
+      		redirect_to trainer_path(current_trainer.id)
+      	else
+      		redirect_to pokemons_new_path
+      	end
+  	end
+
+  	def new
+  		@pokemon = Pokemon.new
+  	end
+
+  	private
+
+  	def pokemon_params
+    	params.require(:pokemon).permit(:name) 
+  	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root to: 'home#index'
   patch '/pokemons/:id', to: 'pokemons#capture', as: 'capture'
   patch 'damage', to: 'pokemons#damage', as: 'damage'
+  get "pokemons/new", to: "pokemons#new"
+  post "pokemons", to: "pokemons#create"
   devise_for :trainers
   resources :trainers
 end
-
